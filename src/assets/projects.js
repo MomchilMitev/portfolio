f218e192d89f3de8b663eb914c4348fe64787464
momchilmitev <momchilmitev87@gmail.com>
2021-04-18 12:12:26 +0200

    added new project

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,24 +1,31 @@
 const projects = [
   {
-    name: 'Officelite',
+    name: "Officelite",
     description:
-      'My challenge was to build out this 2-page coming soon website and get it looking as close to the design as possible.',
-    url: 'https://momchilmitev.github.io/officelite/',
-    thumbnail: 'images/projects/officelite.jpg',
+      "My challenge was to build out this 2-page coming soon website and get it looking as close to the design as possible.",
+    url: "https://momchilmitev.github.io/officelite/",
+    thumbnail: "images/projects/officelite.jpg",
   },
   {
-    name: 'IP Address Tracker',
+    name: "IP Address Tracker",
     description:
-      'In this challenge, I used two separate APIs together to create an IP Address Tracking app.',
-    url: 'https://momchilmitev.github.io/ip-tracker/',
-    thumbnail: 'images/projects/ipTracker.jpg',
+      "In this challenge, I used two separate APIs together to create an IP Address Tracking app.",
+    url: "https://momchilmitev.github.io/ip-tracker/",
+    thumbnail: "images/projects/ipTracker.jpg",
   },
   {
-    name: 'Clock App',
+    name: "Clock App",
     description:
-      'In this challenge, I used external APIs together to create a clock app.',
-    url: 'https://momchilmitev.github.io/clock-app/',
-    thumbnail: 'images/projects/clock-app.jpg',
+      "In this challenge, I used external APIs together to create a clock app.",
+    url: "https://momchilmitev.github.io/clock-app/",
+    thumbnail: "images/projects/clock-app.jpg",
+  },
+  {
+    name: "Invoice App",
+    description:
+      "In this challenge, I used vue, express and mongodb together to create a fullstack application for managing invoices.",
+    url: "https://invoice-app-one.vercel.app/",
+    thumbnail: "images/projects/invoice-app.jpg",
   },
 ];
 
